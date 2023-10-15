cdc1d8a68bdbc0dfa2f6f77ba11521c02ae3694d
shortCV <82280581+shortCV@users.noreply.github.com>
2023-10-14 20:42:40 -0400

    Update
    Update

--- PP Backend/testData/testApp/models.py
+++ PP Backend/testData/testApp/models.py
@@ -5,5 +5,3 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
-    out = models.BooleanField(default=False)
-    released = models.DateTimeField
