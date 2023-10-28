f7547524765026d29c5aded4d055d328ea8618bc
shortCV <82280581+shortCV@users.noreply.github.com>
2023-10-28 00:33:52 -0400

    comments

--- PP Backend/testData/testApp/forms.py
+++ PP Backend/testData/testApp/forms.py
@@ -6,15 +6,17 @@ from django.contrib.auth.models import User
 # Create your forms here.
 
 class NewUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True)  # required to create a user account
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "email", "password1", "password2")  # fields that appear on register form (password2 is validation)
 
-    def save(self, commit=True):
+    def save(self, commit=True):  # saves the email to user
         user = super(NewUserForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
+        user.email = self.cleaned_data['email']  # render info to form
+
         if commit:
             user.save()
+
         return user
--- PP Backend/testData/testApp/views.py
+++ PP Backend/testData/testApp/views.py
@@ -9,11 +9,11 @@ from django.contrib.auth.forms import AuthenticationForm
 
 def search(request):
     searched = request.POST['search']  # get inputted search text (i.e. what you're searching for)
-    songs = Songs.objects.filter(title__icontains=searched)
-    artists = Artists.objects.filter(name__icontains=searched)
-    albums = Albums.objects.filter(title__icontains=searched)
+    songs = Songs.objects.filter(title__icontains=searched)  # any songs that contain the inputted search
+    artists = Artists.objects.filter(name__icontains=searched)  # any artists that contain the inputted search
+    albums = Albums.objects.filter(title__icontains=searched)  # any albums that contain the inputted search
 
-    if request.method == 'POST':
+    if request.method == 'POST':  # when user submits search
         return render(request, 'search_direct.html', {'searched': searched, 'songs': songs, 'artists': artists,
                                                       'albums': albums})  # pass the search results through dict.
     else:
@@ -28,38 +28,45 @@ def index(request):
 
 
 def register_request(request):
-    if request.method == "POST":
-        form = NewUserForm(request.POST)
+    if request.method == "POST":  # when user is directed to register page
+        form = NewUserForm(request.POST)  # form creates new user
+
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, "Registration successful.")
-            return redirect("index")
+            user = form.save()  # user input is saved
+            login(request, user)  # login user
+            messages.success(request, "Registration successful.")  # user feedback
+            return redirect("index")  # go back to index when form goes through
+
+        # if form isn't valid
         messages.error(request, "Unsuccessful registration. Invalid information.")
+
     form = NewUserForm()
     return render(request, 'register.html', context={"register_form": form})
 
 
 def login_request(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
+    if request.method == "POST":  # when user is directed to login page
+        form = AuthenticationForm(request, data=request.POST)  # pre-built django authentication form
+
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
+            user = authenticate(username=username, password=password)  # validates users input of credentials
+
             if user is not None:
-                login(request, user)
-                messages.info(request, f"You are now logged in as {username}.")
-                return redirect("index")
+                login(request, user)  # login user
+                messages.info(request, f"You are now logged in as {username}.")  # user feedback
+                return redirect("index")  # direct back to homepage
             else:
-                messages.error(request, "Invalid username or password.")
+                messages.error(request, "Invalid username or password.")  # if user gets login wrong
         else:
-            messages.error(request, "Invalid username or password.")
+            messages.error(request, "Invalid username or password.")  # if form isn't valid
+
     form = AuthenticationForm()
     return render(request=request, template_name="login.html", context={"login_form": form})
 
 
 def logout_request(request):
-    logout(request)
-    messages.info(request, "You have successfully logged out.")
-    return redirect("index")
+    logout(request)  # logs out user
+    messages.info(request, "You have successfully logged out.")  # user feedback
+    return redirect("index")  # direct user back to homepage
